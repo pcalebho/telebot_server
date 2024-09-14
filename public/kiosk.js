99159e91aaa5969635e3db36f38398c648248949
Caleb Ho <pcalebho@gmail.com>
2024-09-14 02:04:36 -0500

    swith to ddns

--- public/kiosk.js
+++ public/kiosk.js
@@ -10,7 +10,8 @@ let turn = 1.0;
 
 kioskVideo = document.getElementById('remoteVideo');
 
-const ros = new ROSLIB.Ros({ url : 'ws://192.168.1.135:8080' });
+//using a dynamic dns makes it easier to transport between places, as I don't have to reupdate the server code
+const ros = new ROSLIB.Ros({ url : 'ws://telepresencerobot.duckdns.org:8080' });
     ros.on('connection', () => {
     socket.emit('rosbridge status', 'successful')
     console.log("Successful RosBridge Websocket Connection");
