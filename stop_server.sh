594d0c156ffd51c7fe03a855931bbc7c707edd6c
Caleb Ho <pcalebho@gmail.com>
2024-09-15 17:14:05 -0500

    update shell script to correctly kill server

--- stop_server.sh
+++ stop_server.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ -f server.pid ]; then
-  kill $(cat server.pid)
-  rm server.pid
-  echo "Server stopped"
+# Find the PID of the process using port 3000
+pid=$(lsof -t -i:3000)
+
+rm server.pid
+
+# Check if any process was found
+if [ -z "$pid" ]; then
+  echo "No Node.js process found on port 3000."
 else
-  echo "No server is running"
+  # Kill the process
+  echo "Killing process on port 3000 with PID: $pid"
+  kill -9 $pid
+  echo "Process $pid killed."
 fi
